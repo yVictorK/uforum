b959a219edd457104dca8a37325d12a047332a7b
yVictorK <victorkossmann22@gmail.com>
2026-04-05 03:15:42 -0400

    feat: allow passing additional arguments to docker compose in dev script

--- dev.sh
+++ dev.sh
@@ -7,6 +7,6 @@
 echo "🚀 Iniciando UForum em Modo de Desenvolvimento (Hot Reload)..."
 
 # Garante que os containers de prod ou antigos não conflitem
-docker compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+docker compose -f docker-compose.yml -f docker-compose.dev.yml up --build "$@"
 
 # Para rodar em background, use: ./dev.sh -d
